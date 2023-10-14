94eec0adf15335490855ddeb57b5f477fa074e76
Illia Belikau <ilasemcenkov1@gmail.com>
2023-10-14 00:07:53 -1100

    Last commit

--- src/main/java/com/example/bank_service/controller/BalanceController.java
+++ src/main/java/com/example/bank_service/controller/BalanceController.java
@@ -2,11 +2,12 @@ package com.example.bank_service.controller;
 
 import com.example.bank_service.service.BankService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import model.TransferBalance;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
-
+@Slf4j
 @RestController("/balance")
 @AllArgsConstructor
 public class BalanceController {
@@ -31,6 +32,14 @@ public class BalanceController {
     @PostMapping("/transfer")
     public void transfer(@RequestBody TransferBalance transferBalance) {
         bankService.makeTransfer(transferBalance);
+
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String handle(IllegalArgumentException e) {
+        log.error(e.getMessage());
+        return "I'm broke!!!";
+
 
     }
 
--- src/main/java/com/example/bank_service/service/BankService.java
+++ src/main/java/com/example/bank_service/service/BankService.java
@@ -1,21 +1,39 @@
 package com.example.bank_service.service;
 
+import com.example.bank_service.repository.BalanceRepository;
+import lombok.AllArgsConstructor;
 import model.TransferBalance;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
 @Service
+@AllArgsConstructor
 public class BankService {
 
+    private final BalanceRepository balanceRepository;
+
     public BigDecimal getBalance(Long accountId) {
-        return null;
+      BigDecimal balance = balanceRepository.getBalanceForId(accountId);
+      if (balance == null) {
+          throw new IllegalArgumentException();
+      }
+      return balance;
     }
 
     public BigDecimal addMoney(Long to, BigDecimal amount) {
-        return null;
+        BigDecimal save = balanceRepository.save(to, amount);
+        return save;
     }
 
     public void makeTransfer(TransferBalance transferBalance) {
+        BigDecimal fromBalance = balanceRepository.getBalanceForId(transferBalance.getFrom());
+        BigDecimal toBalance = balanceRepository.getBalanceForId(transferBalance.getTo());
+        if(fromBalance == null || toBalance == null) throw new IllegalArgumentException();
+        if(transferBalance.getAmount().compareTo(fromBalance) > 0) throw new IllegalArgumentException("No money");
+        BigDecimal updateFromBalance = fromBalance.subtract(transferBalance.getAmount());
+        BigDecimal updateToBalance = toBalance.add(transferBalance.getAmount());
+
+
     }
 }
